fa5b4598e86b0f67b7d2fb9eeb93f4c13e4c6542
CoderJoeW <j.williamson@gambler-labs.com>
2023-06-24 02:13:48 -0400

    udp and auth support

--- SipMaui/SipUserAgent.cs
+++ SipMaui/SipUserAgent.cs
@@ -9,57 +9,126 @@ namespace SipMaui
 {
     public class SipUserAgent
     {
+        public event Action<SipMessage> MessageReceived;
+        public event Action<SipMessage> MessageSent;
+
         public List<SipSession> Sessions { get; set; }
-        public TcpClient Client { get; set; }
+        public TcpClient TcpClient { get; set; }
+        public UdpClient UdpClient { get; set; }
         public string SipServer { get; set; }
         public int SipPort { get; set; }
+        public string UserSipAddress { get; set; }
+        public string TransportProtocol { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
 
-        public SipUserAgent(string sipServer, int sipPort)
+        public CancellationTokenSource ListeningCts { get; private set; }
+
+        public SipUserAgent(string sipServer, int sipPort, string userSipAddress, string transportProtocol, string username, string password)
         {
             Sessions = new List<SipSession>();
             SipServer = sipServer;
             SipPort = sipPort;
-            Client = new TcpClient();
+            UserSipAddress = userSipAddress;
+            TransportProtocol = transportProtocol;
+            Username = username;
+            Password = password;
+            switch (TransportProtocol)
+            {
+                case "tcp":
+                    TcpClient = new TcpClient();
+                    break;
+                case "udp":
+                    UdpClient = new UdpClient();
+                    break;
+                default:
+                    throw new ArgumentException("Unsupported transport protocol. Use either \"tcp\" or \"udp\".");
+            }
+        }
+
+        public void StartListening()
+        {
+            ListeningCts = new CancellationTokenSource();
+
+            Task.Run(async () =>
+            {
+                while (!ListeningCts.IsCancellationRequested)
+                {
+                    await ReceiveMessage();
+                }
+            });
+        }
+
+        public void StopListening()
+        {
+            ListeningCts?.Cancel();
         }
 
         public async Task SendMessage(SipMessage message)
         {
-            if (!Client.Connected)
+            byte[] data = Encoding.ASCII.GetBytes(message.ConstructMessage());
+
+            switch (TransportProtocol)
             {
-                await Client.ConnectAsync(SipServer, SipPort);
+                case "tcp":
+                    if (!TcpClient.Connected)
+                    {
+                        await TcpClient.ConnectAsync(SipServer, SipPort);
+                    }
+
+                    NetworkStream stream = TcpClient.GetStream();
+                    await stream.WriteAsync(data, 0, data.Length);
+                    break;
+                case "udp":
+                    if (!UdpClient.Client.Connected)
+                    {
+                        UdpClient.Connect(SipServer, SipPort);
+                    }
+                    await UdpClient.SendAsync(data, data.Length);
+                    break;
             }
 
-            NetworkStream stream = Client.GetStream();
-
-            var rawMessage = message.ConstructMessage();
-
-            byte[] data = Encoding.ASCII.GetBytes(rawMessage);
-
-            await stream.WriteAsync(data, 0, data.Length);
+            MessageSent?.Invoke(message);
         }
 
         public async Task ReceiveMessage()
         {
-            if (!Client.Connected)
-            {
-                await Client.ConnectAsync(SipServer, SipPort);
-            }
-
-            NetworkStream stream = Client.GetStream();
-
             byte[] data = new byte[256];
-
             string rawMessage = string.Empty;
 
-            int bytes;
-            while((bytes = await stream.ReadAsync(data, 0, data.Length)) != 0)
+            switch (TransportProtocol)
             {
-                rawMessage += Encoding.ASCII.GetString(data, 0, bytes);
+                case "tcp":
+                    if (!TcpClient.Connected)
+                    {
+                        await TcpClient.ConnectAsync(SipServer, SipPort);
+                    }
+
+                    NetworkStream stream = TcpClient.GetStream();
+
+                    int bytes;
+                    while ((bytes = await stream.ReadAsync(data, 0, data.Length)) != 0)
+                    {
+                        rawMessage += Encoding.ASCII.GetString(data, 0, bytes);
+                    }
+                    break;
+                case "udp":
+                    if (!UdpClient.Client.Connected)
+                    {
+                        UdpClient.Connect(SipServer, SipPort);
+                    }
+
+                    UdpReceiveResult result = await UdpClient.ReceiveAsync();
+                    rawMessage = Encoding.ASCII.GetString(result.Buffer);
+                    break;
             }
 
             var message = new SipMessage("", new Dictionary<string, string>(), "");
-
             message.ParseMessage(rawMessage);
+
+            MessageReceived?.Invoke(message);
+
+            HandleReceivedMessage(message);
         }
 
         public void HandleReceivedMessage(SipMessage message)
@@ -90,7 +159,7 @@ namespace SipMaui
                         { "From", message.Headers["To"] },
                         { "CSeq", "1 INVITE" },
                         { "Call-ID", message.Headers["Call-ID"] },
-                        { "Contact", "<sip:alice@atlanta.com;transport=tcp>" },
+                        { "Contact", $"<sip:{UserSipAddress};transport={TransportProtocol}>" },
                         { "Content-Type", "application/sdp" },
                     };
 
@@ -107,7 +176,56 @@ namespace SipMaui
                 case "BYE":
                     session.TerminateSession();
                     break;
+                case "401 Unauthorized":
+                case "407 Proxy Authentication Required":
+                    HandleAuthentication(message);
+                    break;
             }
         }
+
+        public void HandleAuthentication(SipMessage message)
+        {
+            var authenticateHeader = message.Method == "401 Unauthorized" ? "WWW-Authenticate" : "Proxy-Authenticate";
+            var authenticateValue = message.Headers[authenticateHeader];
+            var parameters = authenticateValue.Split(',').Select(param => param.Trim().Split('=')).ToDictionary(parts => parts[0], parts => parts[1].Trim('"'));
+
+            var nonce = parameters["nonce"];
+            var realm = parameters["realm"];
+
+            var ha1 = ComputeMd5Hash($"{Username}:{realm}:{Password}");
+            var ha2 = ComputeMd5Hash($"{message.Method}:{UserSipAddress}");
+            var response = ComputeMd5Hash($"{ha1}:{nonce}:{ha2}");
+
+            var headers = new Dictionary<string, string>
+            {
+                { "To", message.Headers["From"] },
+                { "From", message.Headers["To"] },
+                { "CSeq", "2 " + message.Method },
+                { "Call-ID", message.Headers["Call-ID"] },
+                { "Contact", $"<sip:{UserSipAddress};transport={TransportProtocol}>" },
+                { message.Method == "401 Unauthorized" ? "Authorization" : "Proxy-Authorization", $"Digest username=\"{Username}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{UserSipAddress}\", response=\"{response}\"" },
+            };
+
+            var newMessage = new SipMessage(message.Method, headers, message.Body);
+            SendMessage(newMessage);
+        }
+
+        public string ComputeMd5Hash(string input)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var inputBytes = Encoding.ASCII.GetBytes(input);
+                var hashBytes = md5.ComputeHash(inputBytes);
+
+                var sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
     }
 }
