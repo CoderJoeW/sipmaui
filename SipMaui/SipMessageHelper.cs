4f670ed15a33741b2a370751b7d8f73f0d5711c1
CoderJoeW <j.williamson@gambler-labs.com>
2023-06-27 23:41:27 -0400

    cleanup

--- SipMaui/SipMessageHelper.cs
+++ SipMaui/SipMessageHelper.cs
@@ -8,8 +8,8 @@ namespace SipMaui
 {
     public class SipMessageHelper
     {
-        public const string EXPIRE = "3600";
-        public const string MAX_FORWARDS = "70";
+        private const string EXPIRE = "3600";
+        private const string MAX_FORWARDS = "70";
 
         private Random _random = new Random();
         private SipUserAgent _userAgent;
@@ -19,25 +19,12 @@ namespace SipMaui
             _userAgent = userAgent;
         }
 
-        private Dictionary<string,string> CreateCommonHeaders(string sipServer, int sipPort, string userSipAddress, string transport, string callId = null)
-        {
-            return new Dictionary<string, string>
-            {
-                { "Via", $"SIP/2.0/UDP {sipServer}:{sipPort};branch=z9hG4bK{_random.Next()}" },
-                { "To", $"<{userSipAddress}>" },
-                { "From", $"<{userSipAddress}>" },
-                { "Call-ID", callId ?? Guid.NewGuid().ToString() },
-                { "Contact", $"<{userSipAddress};transport={transport}>" },
-            };
-        }
-
         public async Task Register(string sipServer, int sipPort, string username, string transport)
         {
             var headers = CreateCommonHeaders(sipServer, sipPort, $"sip:{username}@{sipServer}", transport);
             headers["CSeq"] = "1 REGISTER";
 
-            var registerMessage = new SipMessage($"REGISTER sip:{username}@{sipServer}", headers, "");
-            await _userAgent.SendMessage(registerMessage);
+            await SendMessage($"REGISTER sip:{username}@{sipServer}", headers, "");
         }
 
         public async Task AuthenticateRegister(SipMessage message, string sipServer, int sipPort, string username, string userSipAddress, string transport, string realm, string nonce, string opaque, string qop, string nc, string cnonce, string algorithm, string response)
@@ -49,8 +36,25 @@ namespace SipMaui
             headers["Content-Length"] = "0";
             headers[message.Method == "401 Unauthorized" ? "Authorization" : "Proxy-Authorization"] = $"Digest username=\"{username}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{userSipAddress}\", response=\"{response}\", algorithm={algorithm}, opaque=\"{opaque}\", qop={qop}, nc={nc}, cnonce=\"{cnonce}\"";
 
-            var authenticateRegisterMessage = new SipMessage($"REGISTER {userSipAddress}", headers, message.Body);
-            await _userAgent.SendMessage(authenticateRegisterMessage);
+            await SendMessage($"REGISTER {userSipAddress}", headers, message.Body);
+        }
+
+        private Dictionary<string, string> CreateCommonHeaders(string sipServer, int sipPort, string userSipAddress, string transport, string callId = null)
+        {
+            return new Dictionary<string, string>
+            {
+                { "Via", $"SIP/2.0/UDP {sipServer}:{sipPort};branch=z9hG4bK{_random.Next()}" },
+                { "To", $"<{userSipAddress}>" },
+                { "From", $"<{userSipAddress}>" },
+                { "Call-ID", callId ?? Guid.NewGuid().ToString() },
+                { "Contact", $"<{userSipAddress};transport={transport}>" },
+            };
+        }
+
+        public async Task SendMessage(string method, Dictionary<string,string> headers, string body)
+        {
+            var message = new SipMessage(method, headers, body);
+            await _userAgent.SendMessage(message);
         }
     }
 }
