3bc3ffed17a1d577834158f7078b49140b167a11
CoderJoeW <j.williamson@gambler-labs.com>
2023-06-29 20:49:53 -0400

    small console application to test functionality of sip library

---
+++ SipMauiTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using SipMaui;
+using SipMauiTester;
+
+string json = File.ReadAllText("config.json");
+
+SipConfig config = JsonConvert.DeserializeObject<SipConfig>(json);
+
+var userAgent = new SipUserAgent(config.SipServer, config.SipPort, config.UserSipAddress, config.TransportProtocol, config.Username, config.Password);
+SipMessageHelper sipMessageHelper = new SipMessageHelper(userAgent);
+
+userAgent.MessageReceived += (message) =>
+{
+    Console.WriteLine("Received message: " + message.Method);
+};
+
+userAgent.MessageSent += (message) =>
+{
+    Console.WriteLine("Sent message: " + message.Method);
+};
+
+userAgent.StartListeningForSipMessages();
+
+await sipMessageHelper.Register(config.SipServer, config.SipPort, config.Username, config.TransportProtocol);
+
+Console.WriteLine("Press any key to stop listening...");
+Console.ReadKey();
+
+userAgent.StopListeningForSipMessages();
---
+++ SipMauiTester/SipConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SipMauiTester
+{
+    public class SipConfig
+    {
+        public string SipServer { get; set; }
+        public int SipPort { get; set; }
+        public string UserSipAddress { get; set; }
+        public string TransportProtocol { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
