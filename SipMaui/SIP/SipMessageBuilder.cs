9c5559c44c7a8b0362c74610f52619d39b6bd581
CoderJoeW <j.williamson@gambler-labs.com>
2023-07-01 00:29:04 -0400

    refactoring sip message creation into a builder pattern

---
+++ SipMaui/SIP/SipMessageBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SipMaui.SIP
+{
+    public class SipMessageBuilder
+    {
+        private string _method;
+        private Dictionary<string, string> _headers = new Dictionary<string, string>();
+        private string _body;
+
+        public SipMessageBuilder WithMethod(string method)
+        {
+            _method = method;
+
+            return this;
+        }
+
+        public SipMessageBuilder WithHeader(string key, string value)
+        {
+            _headers.Add(key, value);
+
+            return this;
+        }
+
+        public SipMessageBuilder WithHeaders(Dictionary<string, string> headers)
+        {
+            _headers = headers;
+
+            return this;
+        }
+
+        public SipMessageBuilder WithBody(string body)
+        {
+            _body = body;
+
+            return this;
+        }
+
+        public SipMessageBuilder WithCommonHeaders(string sipServer, int sipPort, string userSipAddress, string transport, string callId = null)
+        {
+            _headers["Via"] = $"SIP/2.0/UDP {sipServer}:{sipPort};branch=z9hG4bK{new Random().Next()}";
+            _headers["To"] = $"<{userSipAddress}>";
+            _headers["From"] = $"<{userSipAddress}>";
+            _headers["Call-ID"] = callId ?? Guid.NewGuid().ToString();
+            _headers["Contact"] = $"<{userSipAddress};transport={transport}>";
+
+            return this;
+        }
+
+        public SipMessage Build()
+        {
+            return new SipMessage(_method, _headers, _body);
+        }
+    }
+}
--- SipMaui/SipMessageHelper.cs
+++ SipMaui/SipMessageHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SipMaui.SIP;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,49 +22,43 @@ namespace SipMaui
 
         public async Task Register(string sipServer, int sipPort, string username, string transport)
         {
-            var headers = CreateCommonHeaders(sipServer, sipPort, $"sip:{username}@{sipServer}", transport);
-            headers["CSeq"] = "1 REGISTER";
+            SipMessage sipMessage = new SipMessageBuilder()
+                .WithMethod($"REGISTER sip:{username}@{sipServer}")
+                .WithCommonHeaders(sipServer, sipPort, $"sip:{username}@{sipServer}", transport)
+                .WithHeader("CSeq", "1 REGISTER")
+                .Build();
 
-            await SendMessage($"REGISTER sip:{username}@{sipServer}", headers, "");
+            await _userAgent.SendMessage(sipMessage);
         }
 
         public async Task AuthenticateRegister(SipMessage message, string sipServer, int sipPort, string username, string userSipAddress, string transport, string realm, string nonce, string opaque, string qop, string nc, string cnonce, string algorithm, string response)
         {
-            var headers = CreateCommonHeaders(sipServer, sipPort, userSipAddress, transport, message.Headers["Call-ID"]);
-            headers["CSeq"] = "2 REGISTER";
-            headers["Expires"] = EXPIRE;
-            headers["Max-Forwards"] = MAX_FORWARDS;
-            headers["Content-Length"] = "0";
-            headers[message.Method == "401 Unauthorized" ? "Authorization" : "Proxy-Authorization"] = $"Digest username=\"{username}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{userSipAddress}\", response=\"{response}\", algorithm={algorithm}, opaque=\"{opaque}\", qop={qop}, nc={nc}, cnonce=\"{cnonce}\"";
-
-            await SendMessage($"REGISTER {userSipAddress}", headers, message.Body);
+            SipMessage sipMessage = new SipMessageBuilder()
+                .WithMethod($"REGISTER {userSipAddress}")
+                .WithCommonHeaders(sipServer, sipPort, userSipAddress, transport)
+                .WithHeader("CSeq", "2 REGISTER")
+                .WithHeader("Expires", EXPIRE)
+                .WithHeader("Max-Forwards", MAX_FORWARDS)
+                .WithHeader("Content-Length", "0")
+                .WithHeader(message.Method == "401 Unauthorized" ? "Authorization" : "Proxy-Authorization", $"Digest username=\"{username}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{userSipAddress}\", response=\"{response}\", algorithm={algorithm}, opaque=\"{opaque}\", qop={qop}, nc={nc}, cnonce=\"{cnonce}\"")
+                .Build();
+            
+            await _userAgent.SendMessage(sipMessage);
         }
 
         public async Task RespondWithOk(string sipServer, int sipPort, string username, string transport, bool allowMethods = false)
         {
-            var headers = CreateCommonHeaders(sipServer, sipPort, username, transport);
-            headers["Content-Type"] = "0";
-            if (allowMethods) headers["Allow"] = "INVITE, ACK, BYE, CANCEL, OPTIONS, MESSAGE, UPDATE, INFO, REGISTER";
+            SipMessageBuilder builder = new SipMessageBuilder()
+                .WithMethod("SIP/2.0 200 OK")
+                .WithCommonHeaders(sipServer, sipPort, username, transport)
+                .WithHeader("Content-Type", "0");
 
-            await SendMessage("SIP/2.0 200 OK", headers, "");
-        }
-
-        private Dictionary<string, string> CreateCommonHeaders(string sipServer, int sipPort, string userSipAddress, string transport, string callId = null)
-        {
-            return new Dictionary<string, string>
+            if (allowMethods)
             {
-                { "Via", $"SIP/2.0/UDP {sipServer}:{sipPort};branch=z9hG4bK{_random.Next()}" },
-                { "To", $"<{userSipAddress}>" },
-                { "From", $"<{userSipAddress}>" },
-                { "Call-ID", callId ?? Guid.NewGuid().ToString() },
-                { "Contact", $"<{userSipAddress};transport={transport}>" },
-            };
-        }
-
-        public async Task SendMessage(string method, Dictionary<string,string> headers, string body)
-        {
-            var message = new SipMessage(method, headers, body);
-            await _userAgent.SendMessage(message);
+                builder.WithHeader("Allow", "INVITE, ACK, BYE, CANCEL, OPTIONS, MESSAGE, UPDATE, INFO, REGISTER");
+            }
+            
+            await _userAgent.SendMessage(builder.Build());
         }
     }
 }
